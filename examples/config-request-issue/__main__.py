88c2b1414731d5a368b1c2a94c63144d3739a7a2
Marcel Arns <marcel.arns@moneymeets.com>
2022-05-16 14:38:51 +0200

    WIP
    Signed-off-by: Felix Hoffmann <felix11h.dev@gmail.com>

--- examples/config-request-issue/__main__.py
+++ examples/config-request-issue/__main__.py
@@ -5,11 +5,12 @@ import pulumi
 from resources.local_module import import_module_read_config
 from resources.local_provider import ConfigRequestTest
 
-print("__main__.py reports: ", pulumi.Config("example").require(key="value"))
+config = pulumi.Config("example").require(key="value")
+print("__main__.py reports: ", config)
 
 import_module_read_config()
 
 if os.environ.get("USE_PROVIDER"):
-    ConfigRequestTest(name=uuid4().hex)
+    ConfigRequestTest(name=uuid4().hex, repository=f"{config}/test123")
 
 
--- examples/config-request-issue/resources/local_provider.py
+++ examples/config-request-issue/resources/local_provider.py
@@ -7,17 +7,17 @@ from pulumi import ResourceOptions, dynamic
 
 class ConfigRequestTestProvider(dynamic.ResourceProvider):
 
-    def create(self, inputs: Any):
-        print("Local provider reports: ", pulumi.Config("example").require(key="value"))
+    def create(self, props: Any):
+        print(props)
         return dynamic.CreateResult(id_=uuid4().hex, outs={})
 
 
 class ConfigRequestTest(dynamic.Resource):
-    def __init__(self, name: str, opts: Optional[ResourceOptions] = None):
 
+    def __init__(self, name: str, repository: pulumi.Input[str], opts: Optional[ResourceOptions] = None):
         super().__init__(
             ConfigRequestTestProvider(),
             name,
-            {},
+            props={"repo": repository},
             opts=opts,
         )
